38d201af442c1af401120cab9d6a6797947877ea
Ded-Codes <160781014+Ded-Codes@users.noreply.github.com>
2024-03-01 00:30:03 +0530

    Create useContext.js
    useContext API- Solution to Prop drilling(let's you teleport prop without passing them down to every child)

---
+++ useContext.js
@@ -0,0 +1,45 @@
+
+import { useContext, useState } from "react";
+import { CountContext } from "./context";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
+    </div>
+  );
+};
+
+function Count({setCount}) {
+  return <div>
+    <CountRenderer />
+    <Buttons setCount={setCount}/>
+    </div>
+}
+
+function CountRenderer(){
+  const count=useContext(CountContext); 
+  return <div>
+      {count}
+    </div>
+}
+
+function Buttons({setCount}) {
+  const count=useContext(CountContext)
+  return (
+    <div>
+      <button onClick={() => {
+        setCount(count+1)
+        }}>Increase</button>
+
+      <button onClick={() => {
+        setCount(count-1)
+        }}>Decreament</button>
+    </div>
+  );
+}
+
+export default App;
